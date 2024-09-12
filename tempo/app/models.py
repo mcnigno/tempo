becece127e9600c8da9bab1bd0611ccd77b8a778
Mc Nigno <mcnigno@gmail.com>
2024-09-12 16:04:41 +0200

    image size fix

--- tempo/app/models.py
+++ tempo/app/models.py
@@ -304,7 +304,6 @@ class Task(Model, AuditMixin):
     @renders('end')
     def task_end(self):
         return date_csm(self.end)
-    
 
 class Step(Model, AuditMixin):
     id = Column(Integer, primary_key=True)
@@ -315,7 +314,7 @@ class Step(Model, AuditMixin):
     projecttask = relationship(Projecttask, backref='Steps')
     input_required = Column(String(100)) 
     completed = Column(Boolean, default=False)
-    photo = Column(ImageColumn(size=(1100, 700, True), thumbnail_size=(64, 48, True)))
+    photo = Column(ImageColumn(size=(1400, 700, True), thumbnail_size=(64, 48, True)))
 
     def photo_img(self): 
         im = ImageManager()
